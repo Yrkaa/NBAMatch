7bbae043ba82c453a14f093ae35be1d2d0f7f7d3
Yrka <varnack30@gmail.com>
2024-01-06 20:23:37 +0300

    Сделал базовые разметку и код

---
+++ app/src/main/java/com/example/nbamatch/MatchList.java
@@ -0,0 +1,21 @@
+package com.example.nbamatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MatchList extends AppCompatActivity {
+
+    //Создание переменных для эл. разметки
+    ListView matchList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.match_list_layout);
+
+        //Инициализация переменных для эл. разметки
+        matchList = findViewById(R.id.match_list);
+    }
+}

c5bebe33de48e0e876a6d3bdbf54ab7fb6213159
Yrka <varnack30@gmail.com>
2024-01-12 17:29:51 +0300

    Сделал проверку на наличие интернета

--- app/src/main/java/com/example/nbamatch/LoadMatchesList.java
+++ app/src/main/java/com/example/nbamatch/LoadMatchesList.java
@@ -2,10 +2,13 @@ package com.example.nbamatch;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,66 +28,91 @@ public class LoadMatchesList extends Thread {
     Activity a;
     ListView list;
     ProgressBar progressBar;
+    TextView title, err;
+
+    ConnectivityManager cm;
 
 
-    public LoadMatchesList(Activity a, ListView list, ProgressBar progressBar){
+    public LoadMatchesList(Activity a, ListView list, ProgressBar progressBar, TextView title, TextView err, ConnectivityManager cm){
         this.a = a;
         this.list = list;
         this.progressBar = progressBar;
+        this.title = title;
+        this.err = err;
+        this.cm = cm;
     }
 
     @Override
     public void run() {
         super.run();
 
-        //Url api с матчами
-        String url = "https://www.balldontlie.io/api/v1/games";
-
-        try {
-            //Подключаемся к серверу
-            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.connect();
-
-            if(connection.getResponseCode() == HttpURLConnection.HTTP_OK){
-                //Получаем данные с сервера
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-                //Преобразуем данные с сервера в JSONObject
-                JSONObject json = new JSONObject(reader.readLine());
-                data = json.getJSONArray("data");
-
-
-                //Заполняем список матчей
-                a.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        ArrayList<String> matchesMassive = new ArrayList<>();
-                        //Проходимся по каждому матчу и вытягиваем названия команд
-                        for(int i = 0; i < data.length(); i++){
-                            try {
-                                String homeTeam = data.getJSONObject(i).getJSONObject("home_team").getString("full_name");
-                                String visitorTeam = data.getJSONObject(i).getJSONObject("visitor_team").getString("full_name");
-                                matchesMassive.add(homeTeam + " - " + visitorTeam);
-                            } catch (JSONException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-
-                        //Скрываем прогресс бар, когда все загрузили
-                        progressBar.setVisibility(View.INVISIBLE);
-
-                        //Создаем адаптор и заполняем список
-                        ArrayAdapter adapter = new ArrayAdapter(a, android.R.layout.simple_list_item_1, matchesMassive);
-                        list.setAdapter(adapter);
-                    }
-                });
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
+        //Проверка на наличие интернета
+        NetworkInfo activeNetwork = null;
+        if (cm != null) {
+            activeNetwork = cm.getActiveNetworkInfo();
         }
+        if (activeNetwork != null && activeNetwork.isConnectedOrConnecting()) {
+//Url api с матчами
+            String url = "https://www.balldontlie.io/api/v1/games";
+
+            try {
+                //Подключаемся к серверу
+                HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+                connection.connect();
+
+                if(connection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                    //Получаем данные с сервера
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                    //Преобразуем данные с сервера в JSONObject
+                    JSONObject json = new JSONObject(reader.readLine());
+                    data = json.getJSONArray("data");
+
+
+                    //Заполняем список матчей
+                    a.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ArrayList<String> matchesMassive = new ArrayList<>();
+                            //Проходимся по каждому матчу и вытягиваем названия команд
+                            for(int i = 0; i < data.length(); i++){
+                                try {
+                                    String homeTeam = data.getJSONObject(i).getJSONObject("home_team").getString("full_name");
+                                    String visitorTeam = data.getJSONObject(i).getJSONObject("visitor_team").getString("full_name");
+                                    matchesMassive.add(homeTeam + " - " + visitorTeam);
+                                } catch (JSONException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+
+                            //Скрываем прогресс бар, когда все загрузили
+                            progressBar.setVisibility(View.INVISIBLE);
+
+                            //Создаем адаптор и заполняем список
+                            ArrayAdapter adapter = new ArrayAdapter(a, android.R.layout.simple_list_item_1, matchesMassive);
+                            list.setAdapter(adapter);
+                        }
+                    });
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else{
+            a.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    list.setVisibility(View.INVISIBLE);
+                    progressBar.setVisibility(View.INVISIBLE);
+                    title.setVisibility(View.INVISIBLE);
+                    err.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+
 
     }
 
--- app/src/main/java/com/example/nbamatch/MatchList.java
+++ app/src/main/java/com/example/nbamatch/MatchList.java
@@ -2,13 +2,16 @@ package com.example.nbamatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MatchList extends AppCompatActivity {
@@ -16,6 +19,7 @@ public class MatchList extends AppCompatActivity {
     //Создание переменных для эл. разметки
     ListView matchList;
     ProgressBar progressBar;
+    TextView title, err;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +29,14 @@ public class MatchList extends AppCompatActivity {
         //Инициализация переменных для эл. разметки
         matchList = findViewById(R.id.match_list);
         progressBar = findViewById(R.id.progressBar);
+        title = findViewById(R.id.title_tv);
+        err = findViewById(R.id.err_tv);
+
+        //Получаем менеджера подключений
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
 
         //Запускаем поток для заполнения списка матчей
-        LoadMatchesList loadMatchesList = new LoadMatchesList(MatchList.this, matchList, progressBar);
+        LoadMatchesList loadMatchesList = new LoadMatchesList(MatchList.this, matchList, progressBar, title, err, cm);
         loadMatchesList.start();
 
         matchList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
